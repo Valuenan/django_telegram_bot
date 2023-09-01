7e484d47f6037a70e8b35512542ee6f69985848a
Vasker <v92prog@gmail.com>
2023-09-01 15:59:53 +0300

    fix

--- shop/telegram/banking_check.py
+++ shop/telegram/banking_check.py
@@ -1,5 +1,5 @@
 from shop.telegram.banking import avangard_check
-from shop.telegram.bot import ready_order_message, send_message_to_user
+from shop.telegram.bot import ready_order_message
 from shop.telegram.db_connection import get_waiting_payment_orders, order_payed
 
 
@@ -18,5 +18,11 @@ def check_orders_payment(session=None):
             if status.strip() == "Оплата поступила":
                 order_payed(order_id)
                 ready_order_message(chat_id=user, order_id=order_id, status='2')
+    else:
+        if session:
+            session.close()
+            session = None
+        else:
+            session = None
 
     return session
