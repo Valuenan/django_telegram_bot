f1458698b873c14967d5d9d7fb37b1d912ebbe1a
Vasker <v92prog@gmail.com>
2023-09-15 22:09:13 +0300

    fix banking_check.py

--- shop/telegram/banking_check.py
+++ shop/telegram/banking_check.py
@@ -17,23 +17,24 @@ def check_orders_payment():
         checked_orders_id = avangard_check(orders_id=orders_id)
 
         for order_id_, data in checked_orders_id.items():
-            payed = int(data[0].replace(' ', ''))
-            order_id, user, products_price, deliver, delivery_price, payment_url, extra_payment_url = data[1]
-            products_price = int(products_price)
-            order_sum = products_price+delivery_price
-            sleep(1)
-            if payed and extra_payment_url is None:
-                if payed == products_price:
-                    if deliver:
-                        order_payed(set_str='payed="1"', order_id=order_id)
+            if data[0]:
+                payed = int(data[0].replace(' ', ''))
+                order_id, user, products_price, deliver, delivery_price, payment_url, extra_payment_url = data[1]
+                products_price = int(products_price)
+                order_sum = products_price+delivery_price
+                sleep(1)
+                if payed and extra_payment_url is None:
+                    if payed == products_price:
+                        if deliver:
+                            order_payed(set_str='payed="1"', order_id=order_id)
+                            ready_order_message(chat_id=user, order_id=order_id, status='2', deliver=deliver, order_sum=order_sum)
+                        else:
+                            order_payed(set_str='payed="1"', order_id=order_id)
+                            ready_order_message(chat_id=user, order_id=order_id, status='4', deliver=deliver, order_sum=order_sum)
+                    elif delivery_price != 0 and payed == products_price + delivery_price:
+                        order_payed(set_str='payed="1", payed_delivery="1"', order_id=order_id)
                         ready_order_message(chat_id=user, order_id=order_id, status='2', deliver=deliver, order_sum=order_sum)
-                    else:
-                        order_payed(set_str='payed="1"', order_id=order_id)
-                        ready_order_message(chat_id=user, order_id=order_id, status='4', deliver=deliver, order_sum=order_sum)
-                elif delivery_price != 0 and payed == products_price + delivery_price:
-                    order_payed(set_str='payed="1", payed_delivery="1"', order_id=order_id)
+                else:
+                    order_payed(set_str='payed_delivery="1"', order_id=order_id)
                     ready_order_message(chat_id=user, order_id=order_id, status='2', deliver=deliver, order_sum=order_sum)
-            else:
-                order_payed(set_str='payed_delivery="1"', order_id=order_id)
-                ready_order_message(chat_id=user, order_id=order_id, status='2', deliver=deliver, order_sum=order_sum)
 
