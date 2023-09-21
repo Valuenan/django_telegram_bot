9ac5865719407e6032b096734a77fabbeb00bfc6
Vasker <v92prog@gmail.com>
2023-09-21 14:48:27 +0300

    fix RemoveDuplicates

--- shop/views.py
+++ shop/views.py
@@ -220,7 +220,8 @@ class RemoveDuplicates(View):
                     decision[duplicate]['rests'] = True
                 if Carts.objects.filter(product=duplicate):
                     decision[duplicate]['cart'] = True
-                if ['rests', 'cart'] in decision[duplicate].values():
+                decision_keys = decision[duplicate].keys()
+                if 'rests' in decision_keys or 'cart' in decision_keys:
                     decision[duplicate]['main'] = True
                     main_duplicate.append(duplicate)
                     main += 1
