36a4728b86b8328c8ad5ec6ab9cffb911a951042
Vasker <v92prog@gmail.com>
2023-11-04 00:06:10 +0300

    fix celery connection

--- django_telegram_bot/settings.py
+++ django_telegram_bot/settings.py
@@ -173,13 +173,13 @@ LOGGING = {
 }
 
 # REDIS related settings
-REDIS_HOST = env("REDIS_HOST", "127.0.0.1"),
+REDIS_HOST = env.str("REDIS_HOST", "127.0.0.1"),
 REDIS_PORT = '6379'
 
 # CELERY broker settings
-CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_BROKER_URL = 'redis://' + f'{REDIS_HOST[0]}' + ':' + REDIS_PORT + '/0'
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
-CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_RESULT_BACKEND = 'redis://' + f'{REDIS_HOST[0]}' + ':' + REDIS_PORT + '/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
